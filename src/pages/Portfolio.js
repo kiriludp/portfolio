9c3dae3611aa34f773d758ff3afe5f813d70703e
Kiri Kiri <killian.podhajsky@gmail.com>
2023-07-14 18:45:08 -0700

    added portfolio images

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,6 +1,11 @@
+import '../assets/Portfolio.css';
+import brick from '../assets/imgs/brick2.png';
+import meditation from '../assets/imgs/Meditation2.png';
+import horror from '../assets/imgs/horror2.png';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Carousel from 'react-bootstrap/Carousel';
 
 function Portfolio() {
 
@@ -14,6 +19,41 @@ function Portfolio() {
             </Col>
         </Row>
     </Container>
+    <Carousel>
+        <Carousel.Item>
+            <img
+            className='d-block w-100'
+            src={brick}
+            alt="First slide"/>
+        
+        <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>We will put a link here</p>
+        </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+            <img
+            className='d-block w-100'
+            src={meditation}
+            alt="First slide"/>
+        
+        <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>We will put a link here</p>
+        </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+            <img
+            className='d-block w-100'
+            src={horror}
+            alt="First slide"/>
+        
+        <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>We will put a link here</p>
+        </Carousel.Caption>
+        </Carousel.Item>
+    </Carousel>
     </div>
     
     );
--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,6 +1,8 @@
+import '../assets/Resume.css'
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+
 
 function Resume() {
 
@@ -13,6 +15,7 @@ function Resume() {
             </Col>
         </Row>
     </Container>
+    
     </div>
     );
     }
