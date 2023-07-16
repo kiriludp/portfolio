21a086e41c28bc320ef3c205c9d2bb9c11fd7f61
kirikiri <killian.podhajsky@gmail.com>
2023-07-16 16:35:43 -0700

    hover animations complete; text ease in, shadow on hover

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,56 +1,47 @@
-import '../assets/Portfolio.css';
-import brick from '../assets/imgs/brick2.png';
-import meditation from '../assets/imgs/Meditation2.png';
-import horror from '../assets/imgs/horror2.png';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Carousel from 'react-bootstrap/Carousel';
-import {useState} from 'react';
+import "../assets/Portfolio.css";
+import brick from "../assets/imgs/brick2.png";
+import meditation from "../assets/imgs/Meditation2.png";
+import horror from "../assets/imgs/horror2.png";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Carousel from "react-bootstrap/Carousel";
+import { useState } from "react";
 
 function Portfolio() {
-    const [index, setIndex] = useState(0);
+  const [index, setIndex] = useState(0);
 
   const handleSelect = (selectedIndex) => {
     setIndex(selectedIndex);
   };
 
-    return (
-    
-    <div className='portfolio-body'>
-         <Container className="head">
+  return (
+    <div className="portfolio-body">
+      <Container className="head">
         <Row className="justify-content-lg">
-            <Col lg="auto">
-                Portfolio
-            </Col>
+          <Col lg="auto">Portfolio</Col>
         </Row>
-    </Container>
-    <Carousel activeIndex={index} onSelect={handleSelect}>
-      <Carousel.Item>
-        <img
-          className="d-block"
-          src={brick}
-          alt="First slide"
-        />
+      </Container>
+      <Carousel activeIndex={index} onSelect={handleSelect}>
+        <Carousel.Item>
+          <img className="d-block" src={brick} alt="First slide" />
         </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block"
-          src={meditation}
-          alt="Second slide"
-        />
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block"
-          src={horror}
-          alt="Third slide"
-        />
-      </Carousel.Item>
-    </Carousel>
+        <Carousel.Item>
+          <img className="d-block" 
+          src={meditation} 
+          alt="Second slide" />
+          <Carousel.Caption>
+           <p>Meditation App
+            We will have links or something</p> 
+          </Carousel.Caption>
+        </Carousel.Item>
+
+        <Carousel.Item>
+          <img className="d-block" src={horror} alt="Third slide" />
+        </Carousel.Item>
+      </Carousel>
     </div>
-    
-    );
-    }
-    
-    export default Portfolio;
+  );
+}
+
+export default Portfolio;
