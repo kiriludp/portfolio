396548012e75f1e5414f650228e90cd8099b9aea
kirikiri <killian.podhajsky@gmail.com>
2023-07-26 19:36:23 -0700

    added styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,9 @@ import "../assets/Home.css";
 import {info} from "../components/Info";
 import Smol from "../components/Smol";
 import Socials from "../components/Socials";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 
 
@@ -10,28 +13,37 @@ import Socials from "../components/Socials";
 
 function Home() {
     return (
+        <div className="home">
+        <div className="title">
+            Home
+        </div>
+        
         <div className="home-body">
-            <div className="head">
+            <Container className='head'>
+                <Row md={{span: 6}}>
+                    <Col md={{span: 12, offset: 6}}>
                 <h1>Hi Folks! I am {info.firstName}.
                     </h1>
                 <h2>I am a {info.position}.
                     </h2>
-            </div> 
-           <ul>
+                    </Col>
+                </Row>     
+           <ul className="bio">
             {info.miniBio.map((bio, index) => (
                 <Smol key={index} emoji={bio.emoji} text={bio.text} /> )
             )}    
             </ul>
 
-            <ul>
+            <ul className="social">
                 {info.socials.map((social, index) => (
                 <Socials key={index} link={social.link} label={social.label} />)
             )}
             </ul>
             
             
+        </Container>    
+        </div>
         </div>    
-            
             
 
     )
