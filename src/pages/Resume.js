75c22c5264172b4dced5daa04546e7264478f219
kirikiri <killian.podhajsky@gmail.com>
2023-08-17 21:11:46 -0700

    resume timeline

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,67 +1,56 @@
-import React, {useContext} from 'react';
-import Accordion from 'react-bootstrap/Accordion';
-import AccordionContext from 'react-bootstrap/AccordionContext';
-import { useAccordionButton } from 'react-bootstrap/AccordionButton';
+import React from 'react';
+import '../assets/Resume.css';
 import {info} from "../components/Info";
 
-const PINK = 'rgba(255, 192, 203, 0.6)';
-const BLUE = 'rgba(0, 0, 255, 0.6)';
 
-function ContextAwareToggle({ children, eventKey, callback }) {
-  const { activeEventKey } = useContext(AccordionContext);
+  
 
-  const decoratedOnClick = useAccordionButton(
-    eventKey,
-    () => callback && callback(eventKey),
-  );
-  const isCurrentEventKey = activeEventKey === eventKey;
+export default function() {
 
-  return (
-    <button
-      type="button"
-      style={{ backgroundColor: isCurrentEventKey ? PINK : BLUE }}
-      onClick={decoratedOnClick}
-    >
-      {children}
-    </button>
-  );
-}
-
-function Resume() {
-  return (
+  return(
     <div className="res-body">
       <div id="title">
         <h1 id="header">Resume</h1>
       </div>
-    <Accordion flush>
-      <Accordion.Item eventKey="0">
-        <Accordion.Header>Accordion Item #1</Accordion.Header>
-        <Accordion.Body>
-          <h6>Education Experience</h6>
-          {info.education.map((education, index) => (
-          <p key={index}>{education.school}<br/>{education.diploma}</p>))}
-        </Accordion.Body>
-      </Accordion.Item>
-      <Accordion.Item eventKey="1">
-        <Accordion.Header>Accordion Item #2</Accordion.Header>
-        <Accordion.Body>
-          <h6>Work Experience</h6>
-          <Accordion.Item eventKey="2">
-        <Accordion.Header>Accordion Item #1</Accordion.Header>
-        <Accordion.Body>
-          <h6>Education Experience</h6>
-          {info.education.map((education, index) => (
-          <p key={index}>{education.school}<br/>{education.diploma}</p>))}
-        </Accordion.Body>
-      </Accordion.Item>
-        </Accordion.Body>
-      </Accordion.Item>
+      <div className="timeline">
+      <div className="info">
+        <div className="outer">
+          <div className="card">
+            <div className="details">
+            <h6 className="label">Education</h6>
+              {info.education.map((education, index) => (
+              <p key={index}> {education.school}< br/> {education.diploma} </p>
+            ))}
+            </div>
+          </div>
+          <div className="card">
+            <div className="details">
+              <h6 className="label">Experience</h6>
+              <p>Work Experience Here.</p>
+            </div>
+          </div>
+          <div className="card">
+            <div className="details">
+            <h6 className="label">Languages</h6>
+              <ul>
+                <li>Ones I know</li>
+              </ul>
+              <ul>
+                <li>Ones I'm familiar with</li>
+              </ul> 
+            </div>
+          </div>
+          <div className="card">
+            <div className="details">
+            <h6 className="label">Skills</h6>
+              <p>My Skills</p>
+            </div>
+          </div>
+        </div>
       
-    </Accordion>
+      </div>
+      </div>
     </div>
-  );
+  )
+
 }
-
-export default Resume;
-
-    
